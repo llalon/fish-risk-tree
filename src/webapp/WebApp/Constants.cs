e7d72309a84014eea69f40d0e7f19371ac979397
llalon <71791017+llalon@users.noreply.github.com>
2021-12-08 09:28:56 -0500

    Updated github links

--- src/webapp/WebApp/Constants.cs
+++ src/webapp/WebApp/Constants.cs
@@ -4,5 +4,6 @@
     {
         public const string OUTPUT_DIR = "out";
         public const bool DEMO_MODE = true;
+        public const string GITHUB_LINK = "https://github.com/llalon/fish-risk-tree";
     }
 }
